7fd668c2dca79ba9fcf34cf91dc34abbe9b19a52
soya_daizu <mail@daizu.dev>
2024-02-20 18:13:51 +0900

    refactor: remove unnecessary function arguments

--- src/functions/resolveUrl.js
+++ src/functions/resolveUrl.js
@@ -14,19 +14,20 @@ export default async function resolveUrl(url) {
   let spotifyResult;
   const items = [];
 
-  if (ytdl.validateURL(url)) items.push([ytdl.getURLVideoID(url), {}]);
-  else if (ytpl.validateID(url)) {
+  if (ytdl.validateURL(url)) {
+    items.push([ytdl.getURLVideoID(url), {}]);
+  } else if (ytpl.validateID(url)) {
     const playlist = await ytpl(url);
     items.push(...playlist.items.map((item) => [item.id, {}]));
   } else if ((spotifyResult = spotifyRegex.exec(url)) !== null) {
     const [, , type, id] = spotifyResult;
 
     if (type === "track") {
-      items.push(await resolveSpotifyTrack(type, id));
+      items.push(await resolveSpotifyTrack(id));
     } else if (type === "album") {
-      items.push(...(await resolveSpotifyAlbum(type, id)));
+      items.push(...(await resolveSpotifyAlbum(id)));
     } else if (type === "playlist") {
-      items.push(...(await resolveSpotifyPlaylist(type, id)));
+      items.push(...(await resolveSpotifyPlaylist(id)));
     } else return;
   }
 
@@ -40,10 +41,10 @@ function makeProperUrl(type, id) {
   return `https://open.spotify.com/${type}/${id}`;
 }
 
-async function resolveSpotifyTrack(type, id) {
+async function resolveSpotifyTrack(id) {
   const track = await sp.getTrack(id);
   const trackInfo = {
-    url: makeProperUrl(type, id),
+    url: makeProperUrl("track", id),
     title: track.data.trackUnion.name,
     artist: track.data.trackUnion.artistsWithRoles.items
       .map((i) => i.artist.profile.name)
@@ -57,10 +58,10 @@ async function resolveSpotifyTrack(type, id) {
   return [searchResult[0].videoId, trackInfo];
 }
 
-async function resolveSpotifyAlbum(type, id) {
+async function resolveSpotifyAlbum(id) {
   const album = await sp.getAlbum(id);
   const albumInfo = {
-    url: makeProperUrl(type, id),
+    url: makeProperUrl("album", id),
     title: album.data.albumUnion.name,
     artist: album.data.albumUnion.artists.items
       .map((i) => i.profile.name)
@@ -75,7 +76,7 @@ async function resolveSpotifyAlbum(type, id) {
   const items = await Promise.all(
     album.data.albumUnion.tracks.items.map(async ({ track }, i) => {
       const trackInfo = {
-        url: makeProperUrl(type, id),
+        url: makeProperUrl("album", id),
         title: track.name,
         artist: track.artists.items.map((i) => i.profile.name).join(", "),
         thumbnail: album.data.albumUnion.coverArt.sources[0].url,
@@ -88,12 +89,13 @@ async function resolveSpotifyAlbum(type, id) {
   return items;
 }
 
-async function resolveSpotifyPlaylist(type, id) {
+async function resolveSpotifyPlaylist(id) {
   const playlist = await sp.getPlaylist(id);
+
   const items = await Promise.all(
     playlist.data.playlistV2.content.items.map(async ({ item }) => {
       const trackInfo = {
-        url: makeProperUrl(type, id),
+        url: makeProperUrl("playlist", id),
         title: item.data.name,
         artist: item.data.artists.items.map((i) => i.profile.name).join(", "),
         thumbnail: item.data.albumOfTrack.coverArt.sources[0].url,
